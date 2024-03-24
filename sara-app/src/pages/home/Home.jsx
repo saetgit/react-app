9514371c9c8ff4f45f9c30634944501ed7d7f8b4
saetgit <etemadian.sara.1998@gmail.com>
2024-03-24 12:53:21 +0330

    list article with method map

--- sara-app/src/pages/home/Home.jsx
+++ sara-app/src/pages/home/Home.jsx
@@ -2,20 +2,29 @@ import Article from "../../components/article/Article";
 import Navbar from "../../components/navbar/Navbar";
 import styled from "./home.module.css";
 function Home() {
+  let articles = [
+    {
+      id: 1,
+      imageUrl: "",
+      title: "عنوان تستی۱",
+      readingTime: 5,
+    },
+    {
+      id: 2,
+      imageUrl: "",
+      title: "عنوان ggg",
+      readingTime: 4,
+    },
+  ];
   return (
     <div className={styled.homeWrapper}>
       <Navbar title="سارا بلاگ" />
       <div className="container">
         <h2> مقالات جدید</h2>
         <div className={styled.articles}>
-          <Article />
-          <Article />
-          <Article />
-          <Article />
-          <Article />
-          <Article />
-          <Article />
-          <Article />
+          {articles.map((article) => (
+            <Article />
+          ))}
         </div>
       </div>
     </div>
