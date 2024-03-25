e87340bff7296119a6878a9897f13b08f53b03ac
saetgit <etemadian.sara.1998@gmail.com>
2024-03-25 14:00:45 +0330

    fetch info blog with axiosan useEffect

--- sara-app/src/pages/articlePage/ArticlePage.jsx
+++ sara-app/src/pages/articlePage/ArticlePage.jsx
@@ -1,10 +1,42 @@
 import Navbar from "../../components/navbar/Navbar";
+import styled from "./article.module.css";
+import pic from "../../assets/images/article.jpg";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function ArticlePage() {
+  const [article, setArticle] = useState({});
+
+  const param = useParams();
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8000/articles/${param.id}`)
+      .then((result) => {
+        setArticle(result.data)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div>
       <Navbar />
-      Article Page
+      <div className={styled.articlePage}>
+        <div className="container">
+          <h2>{article.title}</h2>
+          <div className={styled.info}>
+            <span>تاریخ:{article.date} </span>
+            <span>نویسنده:{article.author}</span>
+            <span>زمان :{article.readingTime} </span>
+          </div>
+          <img src={article.imageUrl} alt="" />
+          <p>{article.content}</p>
+        </div>
+      </div>
     </div>
   );
 }
